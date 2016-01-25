6f061470feee74ef19aabd086c6c3e04f67a44c8
Austin W Brennan <awbrenn@l0021.fx.clemson.edu>
2016-01-25 11:50:24 -0500

    WIP - Calculating integrals for three spectras (X,Y, and Z)

--- main.cpp
+++ main.cpp
@@ -6,25 +6,78 @@
 using namespace std;
 
 int NUMBER_OF_WAVELENGTHS = 40;
+struct point *srd;
+struct point *spd;
+
+
+float interpolate(float wavelength, point* array)
+{
+  int base_wavelength = (int) wavelength;
+  int index = (base_wavelength - 380)/10;
+  float result;
+
+  if (base_wavelength == 770)
+    result = array[index].y;
+  else
+    result = (array[index+1].y-array[index].y) * (wavelength-array[index].y) + array[index].y;
+
+  return result;
+}
+
+
+float integrate(point* array1, point* array2)
+{
+  float delta_lambda = 0.1;
+  float wavelength = 380.0;
+  int iterations = (int)(((float)(NUMBER_OF_WAVELENGTHS*10))/delta_lambda);
+  float result = 0.0;
+
+  // Calculate N
+  for (int i=0; i<iterations; ++i)
+  {
+    result += interpolate(wavelength, array1) * interpolate(wavelength, array2) * delta_lambda;
+    wavelength += delta_lambda;
+  }
+}
+
+void calculateXYZ(float* X, float* Y, float* Z)
+{
+  float N;
+
+  N = integrate(spd, fy);
+  *X = (1.0/N) * integrate(srd, fx);
+  *Y = (1.0/N) * integrate(srd, fy);
+  *Z = (1.0/N) * integrate(srd, fz);
+}
 
 int main() {
-  cout << "Hello, World!" << endl;
+  float X,Y,Z;
 
   // spectral refelectance distribution
-  struct point *srd = new struct point[NUMBER_OF_WAVELENGTHS];
-  string line;
-  int i = 0;
+  srd = new struct point[NUMBER_OF_WAVELENGTHS];
+  // spectral power distribution
+  spd = new struct point[NUMBER_OF_WAVELENGTHS];
+
+  for (int i=0; i<NUMBER_OF_WAVELENGTHS; ++i) {
+    spd[i].x = 380.0 + i*10;
+    spd[i].y = 1.0;
+  }
+  
 
   ifstream myfile("mysteryS16.spectrum");
   if (myfile.is_open()) {
-    while (getline(myfile, line)) {
-      cout << srd[i].x << srd[i].y << '\n';
-      ++i;
+    for (int i=0;i<NUMBER_OF_WAVELENGTHS; ++i) {
+        myfile >> srd[i].x >> srd[i].y;
     }
     myfile.close();
   }
-
   else cout << "Unable to open file";
+
+  cout << srd[10].x << " " << srd[10].y << endl;
+
+  calculateXYZ(&X, &Y, &Z);
+
+  cout << X << " " << Y << " " << Z << endl;
 
   return 0;
 }

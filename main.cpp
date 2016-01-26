9b3927dc2901631fe955112a69fa810c5bb93438
Austin Brennan <awbrenn@clemson.edu>
2016-01-26 18:24:35 -0500

    Finishing up calcualations. Working on finding a bug (Numbers are off).

--- main.cpp
+++ main.cpp
@@ -2,35 +2,45 @@
 #include <fstream>
 
 #include "CIE_matching_functions.h"
+#include "vecmat/Vector.h"
+#include "vecmat/Matrix.h"
 
 using namespace std;
 
 int NUMBER_OF_WAVELENGTHS = 40;
 struct point *srd;
 struct point *spd;
+double xw = 0.31271;
+double yw = 0.32902;
+double xr = 0.690;
+double yr = 0.300;
+double xg = 0.205;
+double yg = 0.715;
+double xb = 0.150;
+double yb = 0.045;
 
 
-float interpolate(float wavelength, point* array)
+double interpolate(double wavelength, point* array)
 {
   int base_wavelength = (int) wavelength;
   int index = (base_wavelength - 380)/10;
-  float result;
+  double result;
 
   if (base_wavelength == 770)
     result = array[index].y;
   else
-    result = (array[index+1].y-array[index].y) * (wavelength-array[index].y) + array[index].y;
+    result = array[index+1].y-array[index].y * (wavelength-array[index].y) + array[index].y;
 
   return result;
 }
 
 
-float integrate(point* array1, point* array2)
+double integrate(point* array1, point* array2)
 {
-  float delta_lambda = 0.1;
-  float wavelength = 380.0;
-  int iterations = (int)(((float)(NUMBER_OF_WAVELENGTHS*10))/delta_lambda);
-  float result = 0.0;
+  double delta_lambda = 0.01;
+  double wavelength = 380.0;
+  int iterations = (int)(((double)(NUMBER_OF_WAVELENGTHS*10))/delta_lambda);
+  double result = 0.0;
 
   // Calculate N
   for (int i=0; i<iterations; ++i)
@@ -38,11 +48,13 @@ float integrate(point* array1, point* array2)
     result += interpolate(wavelength, array1) * interpolate(wavelength, array2) * delta_lambda;
     wavelength += delta_lambda;
   }
+
+  return result;
 }
 
-void calculateXYZ(float* X, float* Y, float* Z)
+void calculateXYZ(double* X, double* Y, double* Z)
 {
-  float N;
+  double N;
 
   N = integrate(spd, fy);
   *X = (1.0/N) * integrate(srd, fx);
@@ -50,8 +62,22 @@ void calculateXYZ(float* X, float* Y, float* Z)
   *Z = (1.0/N) * integrate(srd, fz);
 }
 
+double calculatez(double x, double y)
+{
+  return (1.0-x-y)/y;
+}
+
+void recoverWhite(double* Xw, double* Yw, double* Zw)
+{
+  *Yw = 1.0;
+  *Xw = xw/yw;
+  *Zw = calculatez(xw, yw);
+}
+
+
 int main() {
-  float X,Y,Z;
+  double X,Y,Z;
+  double Xw, Yw, Zw;
 
   // spectral refelectance distribution
   srd = new struct point[NUMBER_OF_WAVELENGTHS];
@@ -62,9 +88,8 @@ int main() {
     spd[i].x = 380.0 + i*10;
     spd[i].y = 1.0;
   }
-  
 
-  ifstream myfile("mysteryS16.spectrum");
+  ifstream myfile("/home/awbrenn/Documents/workspace/6050/1/mysteryS15.spectrum");
   if (myfile.is_open()) {
     for (int i=0;i<NUMBER_OF_WAVELENGTHS; ++i) {
         myfile >> srd[i].x >> srd[i].y;
@@ -73,11 +98,31 @@ int main() {
   }
   else cout << "Unable to open file";
 
-  cout << srd[10].x << " " << srd[10].y << endl;
-
+  // Step 0: compute four integrals for X,Y,Z
   calculateXYZ(&X, &Y, &Z);
 
-  cout << X << " " << Y << " " << Z << endl;
+  // Step 1: recover white Xw, Yw, Zw,
+  recoverWhite(&Xw, &Yw, &Zw);
+
+  // Step 2: solve matrix equation for Cr, Cg, Cb
+  const double zr = calculatez(xr, yr);
+  const double zg = calculatez(xg, yg);
+  const double zb = calculatez(xb, yb);
+  Vector3d XwYwZw(Xw, Yw, Zw);
+  Matrix3x3 xyzrgb(xr, xg, xb, yr, yg, yb, zr, zg, zb);
+  Vector3d CrCgCb = xyzrgb.inv() * XwYwZw;
+
+  // Step 3: Solve matrix equation for R,G,B
+  Vector3d XYZ(X,Y,Z);
+  Matrix3x3 xyzrgbC(xr*CrCgCb[0], xg*CrCgCb[1], xb*CrCgCb[2],
+                    yr*CrCgCb[0], yg*CrCgCb[1], yb*CrCgCb[2],
+                    zr*CrCgCb[0], zg*CrCgCb[1], zb*CrCgCb[2]);
+  Vector3d RGB = xyzrgbC.inv() * XYZ;
+
+  cout << "xyzrgb\n" << xyzrgb << "\n" << endl;
+  cout << "X, Y, Z\n" << XYZ << "\n" << endl;
+  cout << "Cr, Cg, Cb\n" << CrCgCb << "\n" << endl;
+  cout << "RGB\n" << RGB << "\n" << endl;
 
   return 0;
 }
